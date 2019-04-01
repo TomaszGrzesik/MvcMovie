f186fd1a01b5dee5fd096aee8a8c67442796c40d
Wrekkler <t.grzesik19@gmail.com>
2019-04-01 11:15:48 +0200

    Initial migration with EF Core

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -3,11 +3,16 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MvcMovie.Models
 {
+
+    //
+    // Model which allow to create database with attributes such as attributes
     public class Movie
     {
+        // Required by the database for the primary key
         public int Id { get; set; }
         public string Title { get; set; }
 
+        // Allow enter only date info, time is not necessary
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
